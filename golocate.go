85c99799848e5e97f0b73645e3db7ba2ac420104
Andreas Raster <lazor@affenbande.org>
2018-02-16 02:54:40 +0100

    display filenames and modification time
    - also make columns fixed size but resizable by user and clickable

--- golocate.go
+++ golocate.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/go-cmd/cmd"
 	"github.com/gotk3/gotk3/glib"
@@ -25,11 +26,11 @@ func DirToDbName(dir string) string {
 func UpdateDbAndLocate(updatedb string, mlocate string, dir string) []string {
 	dbname := DirToDbName(dir)
 
-	updatedbCmd := cmd.NewCmd(updatedb, "--require-visibility", "0", "-o", "/home/rakete/mlocate/"+dbname+".db", "-U", dir)
+	updatedbCmd := cmd.NewCmd(updatedb, "--require-visibility", "0", "-o", "/dev/shm/"+dbname+".db", "-U", dir)
 	updatedbStatusChan := updatedbCmd.Start()
 	<-updatedbStatusChan
 
-	mlocateCmd := cmd.NewCmd(mlocate, "-d", "/home/rakete/mlocate/"+dbname+".db", "-r", ".*")
+	mlocateCmd := cmd.NewCmd(mlocate, "-d", "/dev/shm/"+dbname+".db", "-r", "go")
 	mlocateStatusChan := mlocateCmd.Start()
 	<-mlocateStatusChan
 
@@ -50,32 +51,32 @@ func Search(liststore *gtk.ListStore) {
 
 	glib.IdleAdd(liststore.Clear)
 
-	directories := []string{"/home/rakete", "/usr", "/var", "/bin", "/lib", "/sys", "/proc"}
+	directories := []string{os.Getenv("HOME")}
 
 	var wg sync.WaitGroup
 	for _, dir := range directories {
 		wg.Add(1)
 		go func(dir string) {
 			stdout := UpdateDbAndLocate(updatedb, mlocate, dir)
-			stdoutlength := fmt.Sprintf("%d", len(stdout))
-			glib.IdleAdd(AddRow, liststore, dir, stdoutlength)
+			for i := 0; i < len(stdout); i++ {
+				fileinfo, err := os.Stat(stdout[i])
+				if err != nil {
+					fmt.Println("Error reading file:", stdout[i], " ", err)
+				} else {
+					t := fileinfo.ModTime()
+					glib.IdleAdd(AddRow, liststore, stdout[i], t.Format(time.UnixDate))
+				}
+			}
 			defer wg.Done()
 		}(dir)
 	}
 
 	wg.Wait()
-
-	glib.IdleAdd(func() {
-		for i := 0; i < 10000; i++ {
-			var iter gtk.TreeIter
-			liststore.InsertWithValues(&iter, -1, []int{COLUMN_FILENAME, COLUMN_COUNT}, []interface{}{"InsertWithValues", i})
-		}
-	})
 }
 
 const (
 	COLUMN_FILENAME = iota
-	COLUMN_COUNT
+	COLUMN_MODTIME
 )
 
 func CreateColumn(title string, id int) *gtk.TreeViewColumn {
@@ -89,14 +90,19 @@ func CreateColumn(title string, id int) *gtk.TreeViewColumn {
 		log.Fatal("Unable to create cell column:", err)
 	}
 
+	column.SetSizing(gtk.TREE_VIEW_COLUMN_FIXED)
+	column.SetResizable(true)
+	column.SetClickable(true)
+	column.SetReorderable(true)
+	column.SetSortIndicator(true)
+
 	return column
 }
 
 func AddRow(liststore *gtk.ListStore, filename, count string) {
-	iter := liststore.Append()
-
-	err := liststore.Set(iter,
-		[]int{COLUMN_FILENAME, COLUMN_COUNT},
+	var iter gtk.TreeIter
+	err := liststore.InsertWithValues(&iter, -1,
+		[]int{COLUMN_FILENAME, COLUMN_MODTIME},
 		[]interface{}{filename, count})
 
 	if err != nil {
@@ -111,7 +117,7 @@ func SetupTreeView() (*gtk.TreeView, *gtk.ListStore) {
 	}
 
 	treeView.AppendColumn(CreateColumn("Filename", COLUMN_FILENAME))
-	treeView.AppendColumn(CreateColumn("Count", COLUMN_COUNT))
+	treeView.AppendColumn(CreateColumn("Modification Time", COLUMN_MODTIME))
 
 	// Creating a list store. This is what holds the data that will be shown on our tree view.
 	liststore, err := gtk.ListStoreNew(glib.TYPE_STRING, glib.TYPE_STRING)
@@ -209,7 +215,6 @@ func SetupWindow(application *gtk.Application, treeview *gtk.TreeView, liststore
 }
 
 func main() {
-
 	const appID = "com.github.rakete.golocate"
 	application, err := gtk.ApplicationNew(appID, glib.APPLICATION_FLAGS_NONE)
 	if err != nil {
