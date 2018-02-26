c00a5e763f59f8aaf726a28cffd2feb85eb56c7e
Andreas Raster <lazor@affenbande.org>
2018-02-26 19:38:53 +0100

    implement sort and merge
    - sort by name or modification time per directory, then merge all sorted
      results together

--- search.go
+++ search.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	//"time"
 	"io/ioutil"
-	"syscall"
+	"sort"
 
 	"gotk3/gtk"
 )
@@ -45,23 +45,102 @@ func visit(wg *sync.WaitGroup, maxproc chan struct{}, dirchan chan string, colle
 				files = append(files, FileEntry{path: entrypath, fileinfo: fileinfo, urgency: 0})
 			}
 		}
-		collect <- files
+
+		collect <- sortFileEntries(sorttype, files)
 	}
 
 	defer wg.Done()
 }
 
+type SortFunc func([]FileEntry) []FileEntry
+
+type ByName []FileEntry
+
+func (a ByName) Len() int           { return len(a) }
+func (a ByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByName) Less(i, j int) bool { return a[i].fileinfo.Name() <= a[j].fileinfo.Name() }
+
+type ByModTime []FileEntry
+
+func (a ByModTime) Len() int      { return len(a) }
+func (a ByModTime) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByModTime) Less(i, j int) bool {
+	return a[i].fileinfo.ModTime().Before(a[j].fileinfo.ModTime()) || a[i].fileinfo.ModTime().Equal(a[j].fileinfo.ModTime())
+}
+
+const (
+	SORT_BY_NAME = iota
+	SORT_BY_MODTIME
+)
+
+func sortFileEntries(sorttype int, files []FileEntry) []FileEntry {
+	switch sorttype {
+	case SORT_BY_NAME:
+		sort.Sort(ByName(files))
+	case SORT_BY_MODTIME:
+		sort.Sort(ByModTime(files))
+	}
+	return files
+}
+
+func merge(sorttype int, left, right []FileEntry) []FileEntry {
+	if len(left) == 0 {
+		return right
+	}
+
+	if len(right) == 0 {
+		return left
+	}
+
+	var result []FileEntry
+
+	type LessCompareFunc func([]FileEntry, int, []FileEntry, int) bool
+	var lesscomparefunc LessCompareFunc
+
+	switch sorttype {
+	case SORT_BY_NAME:
+		lesscomparefunc = func(a []FileEntry, i int, b []FileEntry, j int) bool {
+			return a[i].fileinfo.Name() <= b[j].fileinfo.Name()
+		}
+	case SORT_BY_MODTIME:
+		lesscomparefunc = func(a []FileEntry, i int, b []FileEntry, j int) bool {
+			return a[i].fileinfo.ModTime().Before(b[j].fileinfo.ModTime()) || a[i].fileinfo.ModTime().Equal(b[j].fileinfo.ModTime())
+		}
+	}
+
+	if lesscomparefunc(left, 0, right, len(right)-1) {
+		result = append(left, right...)
+	} else if lesscomparefunc(left, len(left)-1, right, 0) {
+		result = append(right, left...)
+	} else {
+		i, j := 0, 0
+		for i < len(left) && j < len(right) {
+			if lesscomparefunc(left, i, right, j) {
+				result = append(result, left[i])
+				i += 1
+			} else {
+				result = append(result, right[j])
+				j += 1
+			}
+		}
+
+		if i < len(left) {
+			result = append(result, left[i:]...)
+		}
+		if j < len(right) {
+			result = append(result, right[j:]...)
+		}
+	}
+
+	return result
+}
+
 func Search(liststore *gtk.ListStore) {
 	log.Println("start search")
 
-	var rLimit syscall.Rlimit
-	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-	if err != nil {
-		log.Println("Error Getting Rlimit ", err)
-	}
+	userDirectories := []string{os.Getenv("HOME"), "/usr", "/sys", "/opt", "/etc", "/bin", "/sbin"}
 
-	userDirectories := []string{os.Getenv("HOME"), "/usr"}
-
+	sorttype := SORT_BY_MODTIME
 	query, err := regexp.Compile(".*")
 	if err != nil {
 		log.Fatal("Error compiling regexp:", err)
@@ -71,7 +150,7 @@ func Search(liststore *gtk.ListStore) {
 	dirchan := make(chan string)
 	collect := make(chan []FileEntry)
 	finish := make(chan struct{})
-	maxproc := make(chan struct{}, rLimit.Cur/2)
+	maxproc := make(chan struct{}, 16)
 	go func() {
 		for {
 			select {
@@ -90,8 +169,8 @@ func Search(liststore *gtk.ListStore) {
 	go func() {
 		for {
 			select {
-			case newEntries := <-collect:
-				results = append(results, newEntries...)
+			case newentries := <-collect:
+				results = merge(sorttype, results, newentries)
 			case <-finish:
 				log.Println(len(results))
 				return
