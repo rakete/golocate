3293468ab507c767bcafcea1ad560c937b4b6501
Andreas Raster <lazor@affenbande.org>
2018-03-03 14:11:18 +0100

    update treeview in intervals with reusults from search

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path"
+	"time"
 
 	"github.com/gotk3/gotk3/glib"
 	"gotk3/gtk"
@@ -32,17 +35,6 @@ func createColumn(title string, id int) *gtk.TreeViewColumn {
 	column.SetSortIndicator(true)
 
 	return column
-}
-
-func AddRow(liststore *gtk.ListStore, filename, count string) {
-	var iter gtk.TreeIter
-	err := liststore.InsertWithValues(&iter, -1,
-		[]int{COLUMN_FILENAME, COLUMN_MODTIME},
-		[]interface{}{filename, count})
-
-	if err != nil {
-		log.Fatal("Unable to add row:", err)
-	}
 }
 
 func setupTreeView() (*gtk.TreeView, *gtk.ListStore) {
@@ -102,27 +94,9 @@ func setupWindow(display ResultChannel, application *gtk.Application, treeview *
 	menu.Append("Search", "app.search")
 	aSearch := glib.SimpleActionNew("search", nil)
 	aSearch.Connect("activate", func() {
-		display := ResultChannel{make(chan []FileEntry), make(chan []FileEntry), make(chan []FileEntry)}
-		finish := make(chan struct{})
 		go func() {
 			glib.IdleAdd(liststore.Clear)
-			Search(display, finish, nil)
-			log.Println("close finish in main")
-		}()
-		var byname, bymodtime, bysize []FileEntry
-		go func() {
-			for {
-				select {
-				case byname = <-display.byname:
-				case bymodtime = <-display.bymodtime:
-				case bysize = <-display.bysize:
-				case <-finish:
-					log.Println("byname:", len(byname))
-					log.Println("bymodtime:", len(bymodtime))
-					log.Println("bysize:", len(bysize))
-					return
-				}
-			}
+			Search(display, nil)
 		}()
 	})
 	application.AddAction(aSearch)
@@ -169,6 +143,88 @@ func setupWindow(display ResultChannel, application *gtk.Application, treeview *
 	win.ShowAll()
 }
 
+func addEntry(liststore *gtk.ListStore, entry FileEntry) gtk.TreeIter {
+	namestring := path.Join(entry.path, entry.fileinfo.Name())
+	sizestring := fmt.Sprintf("%d", entry.fileinfo.Size())
+
+	modtime := entry.fileinfo.ModTime()
+	modtimestring := modtime.Format("2006-01-02 15:04:05")
+
+	var iter gtk.TreeIter
+	err := liststore.InsertWithValues(&iter, -1,
+		[]int{COLUMN_FILENAME, COLUMN_SIZE, COLUMN_MODTIME},
+		[]interface{}{namestring, sizestring, modtimestring})
+
+	if err != nil {
+		log.Fatal("Unable to add row:", err)
+	}
+
+	return iter
+}
+
+func updateEntry(iter *gtk.TreeIter, liststore *gtk.ListStore, entry FileEntry) {
+	namestring := path.Join(entry.path, entry.fileinfo.Name())
+	sizestring := fmt.Sprintf("%d", entry.fileinfo.Size())
+
+	modtime := entry.fileinfo.ModTime()
+	modtimestring := modtime.Format("2006-01-02 15:04:05")
+
+	err := liststore.Set(iter,
+		[]int{COLUMN_FILENAME, COLUMN_SIZE, COLUMN_MODTIME},
+		[]interface{}{namestring, sizestring, modtimestring})
+
+	if err != nil {
+		log.Fatal("Unable to update row:", err)
+	}
+}
+
+func updateView(liststore *gtk.ListStore, display ResultChannel, sorttype chan int) {
+	var byname, bymodtime, bysize []FileEntry
+	currentsort := -1
+
+	go func() {
+		for {
+			select {
+			case <-time.After(1 * time.Second):
+				var entries []FileEntry
+				switch currentsort {
+				case SORT_BY_NAME:
+					entries = byname
+				case SORT_BY_MODTIME:
+					entries = bymodtime
+				case SORT_BY_SIZE:
+					entries = bysize
+				}
+
+				glib.IdleAdd(func() {
+					n := 0
+					iter, valid := liststore.GetIterFirst()
+					for n < len(entries) && valid == true {
+						updateEntry(iter, liststore, entries[n])
+						valid = liststore.IterNext(iter)
+						n += 1
+					}
+					if n < len(entries) && len(entries) > 10000 {
+						for _, entry := range entries[n:10000] {
+							addEntry(liststore, entry)
+							n += 1
+						}
+					}
+				})
+			}
+		}
+	}()
+
+	for {
+		select {
+		case currentsort = <-sorttype:
+		case byname = <-display.byname:
+		case bymodtime = <-display.bymodtime:
+		case bysize = <-display.bysize:
+		}
+	}
+}
+
 func main() {
 	const appID = "com.github.rakete.golocate"
 	application, err := gtk.ApplicationNew(appID, glib.APPLICATION_FLAGS_NONE)
@@ -176,11 +232,20 @@ func main() {
 		log.Fatal("Could not create application:", err)
 	}
 
-	application.Connect("activate", func() {
-		treeview, liststore := SetupTreeView()
-		searchbar := SetupSearchBar()
+	display := ResultChannel{make(chan []FileEntry), make(chan []FileEntry), make(chan []FileEntry)}
 
-		SetupWindow(application, treeview, liststore, searchbar, "golocate")
+	var liststore *gtk.ListStore
+	var treeview *gtk.TreeView
+	var searchbar *gtk.SearchBar
+	sorttype := make(chan int)
+	application.Connect("activate", func() {
+		treeview, liststore = setupTreeView()
+		searchbar = setupSearchBar()
+
+		setupWindow(display, application, treeview, liststore, searchbar, "golocate")
+
+		go updateView(liststore, display, sorttype)
+		sorttype <- SORT_BY_SIZE
 	})
 
 	os.Exit(application.Run(os.Args))
